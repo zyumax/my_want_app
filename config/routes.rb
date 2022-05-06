4bb6d088e1b349b01d80adada8606eb93a8223ef
zyumax <kazuma_sge@icloud.com>
2022-05-06 20:56:28 +0900

    商品登録ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root "products#index"
+  get 'products/index'
+  get 'products/new' 
+  post 'products/new', to: 'products#create'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+
 end
