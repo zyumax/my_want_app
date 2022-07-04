0d9c9c9259eea2a2073918ec91ac10a0008b2d12
zyumax <kazuma_sge@icloud.com>
2022-07-05 00:59:10 +0900

    画像を挿入

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'products#index'
   get 'products/predict'
   resources :products
+  get 'home/home'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
